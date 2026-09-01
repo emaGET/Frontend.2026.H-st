5ec912204c50704a349d6233ee29eda089ae8dbf
emaGET <emanuelepini@getacademy.no>
2026-09-01 13:49:18 +0200

    konsolen viser antall pris (etter rabattene)

--- Emne 2 - Frameworkless, TypeScript og funksjonell programmering/Uke 1/31.08/discount-calculator/main.js
+++ Emne 2 - Frameworkless, TypeScript og funksjonell programmering/Uke 1/31.08/discount-calculator/main.js
@@ -1,3 +1,9 @@
+import {
+    percentDiscount,
+    fixedDiscount,
+    percentDiscountWithMax
+} from "./offers.js" ;
+
 function calculateDiscount() {
     const price = parseFloat(document.getElementById("price").value);
     const selectedDiscount = document.getElementById("discountType").value;
@@ -10,3 +16,8 @@ function calculateDiscount() {
     let newPrice = 0;
     document.getElementById("result").textContent = `Ny pris: ${newPrice.toFixed(2)} kr`;
 }
+
+console.log(percentDiscount(5000));
+console.log(fixedDiscount(5000));
+console.log(percentDiscountWithMax(1000));
+console.log(percentDiscountWithMax(5000));
--- Emne 2 - Frameworkless, TypeScript og funksjonell programmering/Uke 1/31.08/discount-calculator/offers.js
+++ Emne 2 - Frameworkless, TypeScript og funksjonell programmering/Uke 1/31.08/discount-calculator/offers.js
@@ -6,3 +6,9 @@ const fixedDiscount = price => price - 50;
 
 // 40% rabatt, men maks. rabatt 100kr
 const percentDiscountWithMax = price => Math.max(price * 0.6, price - 100);
+
+export {
+    percentDiscount,
+    fixedDiscount,
+    percentDiscountWithMax
+}
