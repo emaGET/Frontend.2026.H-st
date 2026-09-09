81d5db764c825133c452cc56b594a05852ca9fa2
emaGET <emanuelepini@getacademy.no>
2026-09-09 11:09:00 +0200

    more progress

--- Emne 2 - Frameworkless, TypeScript og funksjonell programmering/Uke 1/31.08/clickerNew/js/view.js
+++ Emne 2 - Frameworkless, TypeScript og funksjonell programmering/Uke 1/31.08/clickerNew/js/view.js
@@ -1,35 +1,48 @@
 import { model } from "./model.js";
 import { doClick, buyUpgrade } from "./controller.js";
 
+// function updateView() {
+//     const smiley = model.smileyIndex === 0 ? '😀' : '😁';
+
+//     const upgradeBtnHtml = model.points < 10 ? '' : /*HTML*/`
+//         <button id="upgradeButton">
+//             Kjøp oppgradering (10 poeng)
+//         </button>
+//     `;
+
+//     document.getElementById('app').innerHTML = /*HTML*/`
+//         <div id="image" >${smiley}</div>
+
+//         <div id="pointsInfo">
+//             Poeng: ${model.points}
+//         </div>
+//         ${upgradeBtnHtml}
+//     `;
+
+//     const image = document.getElementById("image");
+
+//     image.addEventListener("click", doClick);
+
+//     const upgradeButton = document.getElementById("upgradeButton");
+
+//     if (upgradeButton) {
+//         upgradeButton.addEventListener("click", buyUpgrade);
+//     }
+// }
+
 function updateView() {
-    const smiley = model.smileyIndex === 0 ? '😀' : '😁';
+    
+    const smileyDiv = document.createElement("div");
 
-    const upgradeBtnHtml = model.points < 10 ? '' : /*HTML*/`
-        <button id="upgradeButton">
-            Kjøp oppgradering (10 poeng)
-        </button>
-    `;
+    smileyDiv.textContent = model.smileyIndex === 0 ? "😀" : "😁";
 
-    document.getElementById('app').innerHTML = /*HTML*/`
-        <div id="image" >${smiley}</div>
+    smileyDiv.addEventListener("click", doClick);
 
-        <div id="pointsInfo">
-            Poeng: ${model.points}
-        </div>
-        ${upgradeBtnHtml}
-    `;
+    const app = document.getElementById("app");
 
-    const image = document.getElementById("image");
-
-    image.addEventListener("click", doClick);
-
-    const upgradeButton = document.getElementById("upgradeButton");
-
-    if (upgradeButton) {
-        upgradeButton.addEventListener("click", buyUpgrade);
-    }
+    app.replaceChildren(smileyDiv);
 }
 
-export { updateView } ;
+export { updateView };
 
 // 😁
